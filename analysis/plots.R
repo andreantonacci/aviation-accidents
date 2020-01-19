ce1b8c08703e2a6fdd586f04363a15e1f6e01fa5
Andrea Antonacci <antonacciad@gmail.com>
2020-01-19 21:10:31 +0100

    Fix date class in time series and add new plots

--- analysis/plots.R
+++ analysis/plots.R
@@ -6,20 +6,79 @@ world_map <- ggplot() +
   coord_fixed(1.3) + 
   theme_void()
 
-col <- c("Minor" = "#ffcca8", "Substantial" = "#F08986", "Destroyed" = "#DC1C13")
+col <- c("Minor" = "#ffcca8", "Substantial" = "#F08986", "Destroyed" = "#DC1C13", "Unknown" = "#d4d4d4")
 plot1 <- world_map + 
   geom_point(data = df_fatal, aes(Longitude, Latitude, color=Aircraft.Damage, size=Total.Fatal.Injuries)) + 
-  scale_color_manual(values = col, breaks = c("Minor", "Substantial", "Destroyed")) + 
-  labs(color = "Aircraft Damage", size = "Total Fatal Injuries") +
-  theme_void()
+  scale_color_manual(values = col, breaks = c("Minor", "Substantial", "Destroyed", "Unknown")) + 
+  labs(title = "Location of Accidents, Aircraft Damage and Number of Fatal Injuries", color = "Aircraft Damage", size = "Total Fatal Injuries") +
+  theme_void() + 
+  theme(plot.title = element_text(vjust=2))
 plot1
 
 # Time series
+
 plot2 <- ggplot(accidents_date, aes(Date, total, group=1)) +
   geom_line() +
-  scale_x_discrete(breaks = unique(accidents_date$Date)[seq(1,500,12)]) + # Show one label per year
-  # scale_x_date(date_breaks = "years" , date_labels = "%Y") + # Oly works if Date is class date (not character)
-  labs(x = "Accident Date", y = "Number of Accidents") +
+  # scale_x_discrete(breaks = unique(accidents_date$Date)[seq(1, 500, 12)]) + # Show one label per year, only works if Date is not class date (a character)
+  scale_x_date(date_breaks = "years" , date_labels = "%Y") +
+  coord_cartesian(xlim = as.Date(c("1983-01-01", "2018-06-01"))) + 
+  labs(title = "Number of Total Accidents per Year", x = "Accident Date", y = "Number of Accidents") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=45, hjust = 1, margin = margin(b = 20)), axis.text.y = element_text(margin = margin(l = 20))) # Rotate x axis labels
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, margin = margin(b = 20)),
+        axis.text.y = element_text(margin = margin(l = 20)),
+        plot.title = element_text(vjust=2))
 plot2
+
+plot3 <- ggplot(fatal_accidents_date, aes(Date, total, group=1)) +
+  geom_line() +
+  # scale_x_discrete(breaks = unique(accidents_date$Date)[seq(1, 500, 12)]) + # Show one label per year, only works if Date is not class date (a character)
+  scale_x_date(date_breaks = "years", date_labels = "%Y") +
+  labs(title = "Number of Fatal Accidents per Year", x = "Accident Date", y = "Number of Accidents") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, margin = margin(t = 5, b = 20)),
+        axis.text.y = element_text(margin = margin(r = 5, l = 20)),
+        plot.title = element_text(vjust=2))
+plot3
+
+plot4 <-  ggplot(fatal_injuries_date, aes(Date, total, group=1)) +
+  geom_line() +
+  scale_x_date(date_breaks = "years" , date_labels = "%Y") +
+  coord_cartesian(xlim = as.Date(c("1983-01-01", "2018-06-01"))) + 
+  labs(title = "Number of Fatal Injuries per Year", x = "Accident Date", y = "Number of Fatal Injuries") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, margin = margin(t = 5, b = 20)),
+        axis.text.y = element_text(margin = margin(r = 5, l = 20)),
+        plot.title = element_text(vjust=2))
+plot4
+
+# Bar charts
+
+plot5 <- ggplot(df, aes(IsFatal, fill = IsFatal)) + 
+  geom_bar() +
+  facet_wrap(~ Broad.Phase.of.Flight, scales = "free") +
+  labs(title = "Number of Fatal and Not Fatal Accidents per Flight Phase", x = "Is the Accident Fatal?", y = "Number of Accidents", fill = "Is the Accident Fatal?") + 
+  theme_bw() +
+  theme(axis.text.x = element_text(margin = margin(t = 5, b = 20)),
+        axis.text.y = element_text(margin = margin(r = 5, l = 20)),
+        plot.title = element_text(vjust=2))
+plot5
+
+# We see that when IMC --> fatal > non fatal
+plot6 <- ggplot(subset(df, !Weather.Condition == "UNK"), aes(Weather.Condition, fill = IsFatal)) + # Ignore UNK weather conditions
+  geom_bar(position = "dodge2") +
+  facet_wrap(~ Broad.Phase.of.Flight, scales = "free") +
+  labs(title = "Number of Accidents per Weather Condition, Fatality and Flight Phase", x = "Weather Condition", y = "Number of Accidents", fill = "Is the Accident Fatal?") + 
+  theme_bw() +
+  theme(axis.text.x = element_text(margin = margin(t = 5, b = 20)),
+        axis.text.y = element_text(margin = margin(r = 5, l = 20)),
+        plot.title = element_text(vjust=2))
+plot6
+
+plot7 <- ggplot(subset(accidents_manufacturer, !Manufacturer == "Other"), aes(Manufacturer, total, fill = IsFatal)) + # Only keep Airbus and Boeing
+  geom_col(position = "dodge2") +
+  labs(title = "Number of Accidents per Manufacturer from January 2000", x = "Manufacturer", y = "Number of Accidents", fill = "Is the Accident Fatal?") + 
+  theme_bw() +
+  theme(axis.text.x = element_text(margin = margin(t = 5, b = 20)),
+        axis.text.y = element_text(margin = margin(r = 5, l = 20)),
+        plot.title = element_text(vjust=2))
+plot7
--- analysis/wrangling.R
+++ analysis/wrangling.R
@@ -6,6 +6,7 @@ library(mapproj)
 library(maps)
 library(lubridate)
 
+# Wrangling ------------
 # rm(list = ls(all.names = TRUE))
 
 df <- read.delim("../data/raw/AviationDataCleaned.csv", sep ="\t", header = TRUE, fill = TRUE, dec =".", strip.white=TRUE) # Remove white trailing with strip.white, if any
@@ -15,21 +16,51 @@ df$Event.Date <- as.Date(df$Event.Date, format = "%m/%d/%Y")
 
 # Update empty cells
 df$Broad.Phase.of.Flight[df$Broad.Phase.of.Flight == ""] <- "UNKNOWN"
+df$Weather.Condition[df$Weather.Condition == ""] <- "UNK"
+df$Aircraft.Damage[df$Aircraft.Damage == ""] <- NA
+
+# Add dummy for fatalities in accident
+df <- mutate(df, IsFatal = ifelse(!Injury.Severity %in% c("Non-Fatal", "Unavailable", "Incident"), "Fatal", "Not Fatal"))
 
 # Retrieve fatal accidents only
 df_fatal <- df %>%
-  filter(Investigation.Type == "Accident" & Aircraft.Category == "Airplane" & !Injury.Severity %in% c("Non-Fatal", "Unavailable") & !Aircraft.Damage == "")
+  filter(Aircraft.Category == "Airplane" & IsFatal == "Fatal")
 
 # Count # of accidents per date
 accidents_date <- df %>%
-  mutate(month = format(Event.Date, "%m"), year = format(Event.Date, "%Y"), Date = format(Event.Date, "%Y-%m")) %>%
+  mutate(Date = format(Event.Date, "%Y-%m")) %>%
   group_by(Date) %>%
   summarize(total = n())
-accidents_date$Date <- as.Date(accidents_date$Date, format = "%Y-%m-%d")
+accidents_date$Date <- as.Date(paste(accidents_date$Date, "-01", sep = ""), format = "%Y-%m-%d") # Transform Date as class date again, adding a fake day
 
-# Exploration
-df_fatal %>%
-  count(Aircraft.Damage, sort=TRUE)
+fatal_accidents_date <- df_fatal %>%
+  mutate(Date = format(Event.Date, "%Y-%m")) %>%
+  group_by(Date) %>%
+  summarize(total = n())
+fatal_accidents_date$Date <- as.Date(paste(fatal_accidents_date$Date, "-01", sep = ""), format = "%Y-%m-%d") # Transform Date as class date again, adding a fake day
 
-df %>%
-  count(Broad.Phase.of.Flight, sort=TRUE)
+# Count # of fatal injuries per date
+fatal_injuries_date <- df_fatal %>%
+  mutate(Date = format(Event.Date, "%Y")) %>%
+  group_by(Date) %>%
+  summarize(total = sum(Total.Fatal.Injuries))
+fatal_injuries_date$Date <- as.Date(paste(fatal_injuries_date$Date, "-01-01", sep = ""), format = "%Y-%m-%d") # Transform Date as class date again, adding a fake day
+
+# Count # of accidents per manufacturer
+accidents_manufacturer <- subset(df, Event.Date > as.Date("2000-01-01")) %>%
+  mutate(Manufacturer = ifelse(Make %in% c("BOEING", "Boeing"), "Boeing", ifelse(Make %in% c("AIRBUS", "Airbus"), "Airbus", "Other"))) %>%
+  group_by(Manufacturer, IsFatal) %>%
+  summarize(total = n())
+
+# Exploration ------------
+
+# Fatal accidents per flight phase
+df_fatal %>% count(Broad.Phase.of.Flight, sort=TRUE)
+
+# Most dangerous airports
+subset(df, !Airport.Code %in% c("", "NONE", "None", "PVT", "N/A")) %>% count(as.character(Airport.Code), sort=TRUE)
+
+# Misc
+# df %>% count(Weather.Condition, sort=TRUE)
+# df %>% count(Make %in% c("AIRBUS", "BOEING", "Boeing", "Airbus"), sort=TRUE)
+# View(subset(df, Make %in% c("AIRBUS", "BOEING", "Boeing", "Airbus") & Event.Date > as.Date("2000-01-01")))
